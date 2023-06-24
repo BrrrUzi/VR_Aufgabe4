d9da137c9d388cf840fcd322d8b7ac246b188354
paddlix <p.floegel@gmx.net>
2023-06-24 15:47:25 +0200

    Script für Ball hinzugefügt

---
+++ VR_Aufgabe04/Assets/Scripts/Schlagzähler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Schlagzähler : MonoBehaviour
+{
+
+    [SerializeField]
+    public TextMeshPro zaehlerText;
+    [SerializeField]
+    public TextMeshPro highScoreText;
+
+    private int zaehler = 0;
+    private int highscore = 0;
+
+    private int i = 0;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setZaehlerText();
+        setHighScoreText();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (rb.velocity.magnitude > 0) {
+            Debug.Log(i++);
+        }
+    }
+
+    private void OnCollisionEnter(Collision col) {
+        if (col.gameObject.CompareTag("Club")) {
+            zaehler++;
+            setZaehlerText();
+        }
+        else if (col.gameObject.CompareTag("Loch")) {
+            if (zaehler < highscore) {
+                highscore = zaehler;
+                setHighScoreText();
+            }
+            
+            zaehler = 0;
+            setZaehlerText();
+        }
+    }
+
+
+
+    private void setZaehlerText() {
+        zaehlerText.text = "Schläge: " + zaehler;
+    }
+
+    private void setHighScoreText() {
+        if (highscore < 0) {
+            highScoreText.text = "Highscore: " + highScoreText;
+        } else {
+            highScoreText.text = "Highscore: none";
+        }
+    }
+}
